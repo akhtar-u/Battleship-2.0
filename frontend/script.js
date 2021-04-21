6a0f00823da1f5541480847f1e4982942d1c788f
Usman Akhtar <akhtar_usman@outlook.com>
2021-04-21 01:23:57 -0400

    add board selection for placing ships

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,99 @@
 "use strict";
-var buttons = /** @class */ (function () {
-    function buttons() {
-        this.currentShip = "";
-        var wrapper = document.getElementById("pships");
-        wrapper.addEventListener("click", function (event) {
-            var isButton = event.target instanceof HTMLButtonElement;
-            if (!isButton) {
-                return;
-            }
-            console.log(event.target.id);
-        });
+let currentShip = null;
+let firstCell = null;
+let secondCell = null;
+let currShipLength = null;
+let carrierCells = new Array(5);
+let battleshipCells = new Array(4);
+let cruiserCells = new Array(3);
+let submarineCells = new Array(3);
+let destroyerCells = new Array(2);
+let allCells = new Array(17);
+/* add event listeners to player buttons and gameboard */
+const pbtnwrapper = document.getElementById("pships");
+const pboardwrapper = document.getElementById("pboard").getElementsByTagName("td");
+pbtnwrapper.addEventListener("click", event => {
+    const isButton = event.target instanceof HTMLButtonElement;
+    if (!isButton) {
+        return;
     }
-    return buttons;
-}());
-var button = new buttons();
+    currentShip = event.target.textContent;
+    shipSelected();
+});
+for (let i = 0; i < pboardwrapper.length; i++) {
+    pboardwrapper[i].addEventListener("click", event => {
+        const isCell = event.target instanceof HTMLTableCellElement;
+        if (!isCell) {
+            return;
+        }
+        if (currentShip != null) {
+            if (firstCell != null) {
+                secondCell = parseInt(event.target.getElementsByTagName("div")[0].id);
+                document.getElementById("message").innerHTML = "You have selected <span>" + secondCell + "</span> " +
+                    "as your ending position.";
+                placeShip();
+            }
+            else {
+                firstCell = parseInt(event.target.getElementsByTagName("div")[0].id);
+                document.getElementById("message").innerHTML = "You have selected <span>" + firstCell + "</span> " +
+                    "as your starting position.";
+            }
+        }
+    });
+}
+function shipSelected() {
+    if (currentShip == "Carrier") {
+        currShipLength = 5;
+    }
+    if (currentShip == "Battleship") {
+        currShipLength = 4;
+    }
+    if (currentShip == "Cruiser") {
+        currShipLength = 3;
+    }
+    if (currentShip == "Submarine") {
+        currShipLength = 3;
+    }
+    if (currentShip == "Destroyer") {
+        currShipLength = 2;
+    }
+    document.getElementById("message").innerHTML = "You have selected: <span>" + currentShip + "</span>." +
+        " It has length, <span>" + currShipLength + "</span>. Click on one of the cells to place your ship.";
+}
+function placeShip() {
+    checkCoordinates();
+    checkOverlap();
+    firstCell = null;
+    secondCell = null;
+}
+function checkCoordinates() {
+    /* check direction and length */
+    if (Math.abs(firstCell - secondCell) < 10) {
+        if (Math.abs(firstCell - secondCell) == currShipLength - 1) {
+            console.log("horizontal");
+        }
+        else {
+            printLengthError();
+        }
+    }
+    else if (Math.abs(firstCell - secondCell) >= 10 && firstCell % 10 == secondCell % 10) {
+        if (Math.abs(firstCell - secondCell) / 10 == currShipLength - 1) {
+            console.log("vertical");
+        }
+        else {
+            printLengthError();
+        }
+    }
+    else {
+        document.getElementById("message").innerHTML = "Your selection is not <span>horizontal</span> " +
+            "or <span>vertical</span>! Try again.";
+    }
+}
+function printLengthError() {
+    document.getElementById("message").innerHTML = "Your selection is not " +
+        "the correct <span>length</span>! Try again.";
+}
+function checkOverlap() {
+    /* if the selection is correct, generate coordinates and check for overlap */
+    // for(let i = firstCell)
+}
--- frontend/script.ts
+++ frontend/script.ts
@@ -1,16 +1,105 @@
-class buttons{
-    currentShip : string = "";
+let currentShip: string | null = null;
+let firstCell: number | null = null;
+let secondCell: number | null = null;
+let currShipLength: number | null = null;
+let carrierCells: number[] = new Array(5);
+let battleshipCells: number[] = new Array(4);
+let cruiserCells: number[] = new Array(3);
+let submarineCells: number[] = new Array(3);
+let destroyerCells: number[] = new Array(2);
+let allCells: number[] = new Array(17);
 
-    constructor() {
-        const wrapper = document.getElementById("pships");
-        wrapper!.addEventListener("click", event => {
-            const isButton = event!.target instanceof HTMLButtonElement;
-            if(!isButton){
-                return;
+/* add event listeners to player buttons and gameboard */
+const pbtnwrapper = document.getElementById("pships");
+const pboardwrapper = document.getElementById("pboard")!.getElementsByTagName("td");
+
+pbtnwrapper!.addEventListener("click", event => {
+    const isButton = event!.target instanceof HTMLButtonElement;
+    if (!isButton) {
+        return;
+    }
+    currentShip = (<HTMLButtonElement>event.target).textContent;
+    shipSelected();
+})
+
+for (let i = 0; i < pboardwrapper.length; i++) {
+    pboardwrapper[i].addEventListener("click", event => {
+        const isCell = event!.target instanceof HTMLTableCellElement;
+        if (!isCell) {
+            return;
+        }
+        if (currentShip != null) {
+            if (firstCell != null) {
+                secondCell = parseInt((<HTMLTableCellElement>event.target).getElementsByTagName("div")[0].id);
+                document.getElementById("message")!.innerHTML = "You have selected <span>" + secondCell + "</span> " +
+                    "as your ending position.";
+                placeShip();
+            } else {
+                firstCell = parseInt((<HTMLTableCellElement>event.target).getElementsByTagName("div")[0].id);
+                document.getElementById("message")!.innerHTML = "You have selected <span>" + firstCell + "</span> " +
+                    "as your starting position.";
             }
-            console.log((<HTMLButtonElement>event.target).id);
-        })
+        }
+    })
+}
+
+function shipSelected() {
+    if (currentShip == "Carrier") {
+        currShipLength = 5;
+    }
+    if (currentShip == "Battleship") {
+        currShipLength = 4;
+    }
+    if (currentShip == "Cruiser") {
+        currShipLength = 3;
+    }
+    if (currentShip == "Submarine") {
+        currShipLength = 3;
+    }
+    if (currentShip == "Destroyer") {
+        currShipLength = 2;
+    }
+
+    document.getElementById("message")!.innerHTML = "You have selected: <span>" + currentShip + "</span>." +
+        " It has length, <span>" + currShipLength + "</span>. Click on one of the cells to place your ship.";
+}
+
+function placeShip() {
+    checkCoordinates();
+    checkOverlap();
+
+    firstCell = null;
+    secondCell = null;
+}
+
+function checkCoordinates() {
+    /* check direction and length */
+    if (Math.abs(firstCell! - secondCell!) < 10) {
+        if (Math.abs(firstCell! - secondCell!) == currShipLength! - 1) {
+            console.log("horizontal");
+        } else {
+            printLengthError();
+        }
+
+    } else if (Math.abs(firstCell! - secondCell!) >= 10 && firstCell! % 10 == secondCell! % 10) {
+        if (Math.abs(firstCell! - secondCell!) / 10 == currShipLength! - 1) {
+            console.log("vertical");
+        } else {
+            printLengthError();
+        }
+    } else {
+        document.getElementById("message")!.innerHTML = "Your selection is not <span>horizontal</span> " +
+            "or <span>vertical</span>! Try again.";
     }
 }
 
-let button = new buttons();
+function printLengthError() {
+    document.getElementById("message")!.innerHTML = "Your selection is not " +
+        "the correct <span>length</span>! Try again.";
+}
+
+function checkOverlap(){
+    /* if the selection is correct, generate coordinates and check for overlap */
+    // for(let i = firstCell)
+
+}
