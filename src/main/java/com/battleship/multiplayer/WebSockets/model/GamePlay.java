001f87c4d2e0f05d64e4ed1ba8dbd700d9ce0789
Usman Akhtar <akhtar_usman@outlook.com>
2021-04-26 04:05:25 -0400

    added websockets and url path mappings

---
+++ src/main/java/com/battleship/multiplayer/WebSockets/model/GamePlay.java
@@ -0,0 +1,9 @@
+package com.battleship.multiplayer.WebSockets.model;
+
+import lombok.Data;
+
+@Data
+public class GamePlay {
+
+    private String gameID;
+}
---
+++ src/main/java/com/battleship/multiplayer/WebSockets/model/GameStatus.java
@@ -0,0 +1,5 @@
+package com.battleship.multiplayer.WebSockets.model;
+
+public enum GameStatus {
+    NEW, IN_PROGRESS, FINISHED
+}
